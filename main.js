554a9e3f75611bffb7c06034f86efeeeb9b927c6
advait27-blip <77043126+advait27-blip@users.noreply.github.com>
2021-02-14 19:36:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function getparagraph1() 
+{
+var inputs = [];
+}
+for(var i = 1 ; i = <=6 ; i++ )
+{
+    input.push(document.getElementById("para_1_1" + i).value);
+    input.push(document.getElementById("para_1_2" + i).value);
+    input.push(document.getElementById("para_1_3" + i).value);
+    input.push(document.getElementById("para_1_4" + i).value);
+    input.push(document.getElementById("para_1_5" + i).value);
+    input.push(document.getElementById("para_1_6" + i).value);
+}
+
+inputs.join(". ");
+document.getElementById("get_paragraph1").innerHTML = inputs.join(". ")
+
+function getparagraph1() 
+{
+var inputs = [];
+}
+for(var i = 2 ; i = <=6 ; i++ )
+{
+ input.push(document.getElementById("para_2_1" + i).value);
+ input.push(document.getElementById("para_2_2" + i).value);
+ input.push(document.getElementById("para_2_3" + i).value);
+ input.push(document.getElementById("para_2_4" + i).value);
+ input.push(document.getElementById("para_2_5" + i).value);
+ input.push(document.getElementById("para_2_6" + i).value);
+
+}
+
+inputs.join(". ");
+document.getElementById("get_paragraph2").innerHTML = inputs.join(". ")
